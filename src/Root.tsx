bc47b8835210ba1fdd07c0c65666502594edf4f1
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-06-22 08:49:19 -0300

    feat(styles): add stylesprovider to fix css specifity

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider as MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from 'styled-components';
+import { StylesProvider } from '@material-ui/styles';
 import App from './App';
 
 const theme = createMuiTheme();
 
 const Root = () => {
   return (
-    <MuiThemeProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </MuiThemeProvider>
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
   )
 }
 
